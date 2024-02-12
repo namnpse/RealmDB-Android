c18dd8ee8e42f0e7d9d52619fd63607a11fa150c
Namnpse <nam444479@gmail.com>
2024-02-12 10:14:36 +0700

    add model classes

---
+++ app/src/main/java/com/namnp/realmandroid/RealmApp.kt
@@ -0,0 +1,9 @@
+package com.namnp.realmandroid
+
+import android.app.Application
+
+class RealmApp: Application()
+
+// Teacher 1-to-1 Address
+// Teacher 1-to-many Course
+// Students many-to-many Course
